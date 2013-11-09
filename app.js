039cb50139308ccc2ebeb67d69f31913442d811e
Volker Tietz <volker.tietz@gmail.com>
2013-11-09 14:33:14 +0100

    added css, mark todos as done

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ var MwayTodo = angular.module('MwayTodo', []);
 MwayTodo.controller('TodosCtrl', function ($scope) {
   $scope.todos = [];
 
+
+  $scope.keyup = function (event) {
+    if (event.keyCode == 13) {
+      $scope.add();
+    }
+  };
+
   $scope.add = function () {
     $scope.todos.push({ title: $scope.newTodo, done: false });
+    $scope.newTodo = '';
   };
 });
