cf9dd1c6b8e68ef5121ca6967ab600631d265534
Volker Tietz <volker.tietz@gmail.com>
2013-11-16 20:18:39 +0100

    refactored todo management into service

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var MwayTodo = angular.module('MwayTodo', []);
 
-MwayTodo.controller('TodosCtrl', function ($scope) {
-  $scope.todos = [];
-
+MwayTodo.controller('TodosCtrl', function ($scope, Todo) {
+  $scope.todos = Todo.todos;
 
   $scope.keyup = function (event) {
     if (event.keyCode == 13) {
@@ -11,7 +10,19 @@ MwayTodo.controller('TodosCtrl', function ($scope) {
   };
 
   $scope.add = function () {
-    $scope.todos.push({ title: $scope.newTodo, done: false });
+    Todo.add($scope.newTodo);
     $scope.newTodo = '';
   };
+});
+
+MwayTodo.service('Todo', function() {
+  var todos = [];
+
+  return {
+    todos: todos,
+
+    add: function(title) {
+      todos.push({ title: title, done: false });
+    }
+  }
 });
