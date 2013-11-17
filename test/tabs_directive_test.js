144b35d7b477f0ebeba2b6136e8c20af8cd27128
Volker Tietz <volker.tietz@gmail.com>
2013-11-17 16:52:20 +0100

    splitted tests into different files

--- test/tabs_directive_test.js
+++ test/tabs_directive_test.js
@@ -1,26 +1,5 @@
 'use strict';
 
-describe('Todo', function () {
-
-  beforeEach(module('MwayTodo'));
-
-  it('should initialize with an empty todos array', inject(function (Todo) {
-    expect(Todo.todos.length).toBe(0);
-  }));
-
-  it('should add todo', inject(function (Todo) {
-    Todo.add('A todo');
-    expect(Todo.todos.length).toBe(1);
-    expect(Todo.todos[0].title).toEqual('A todo');
-    expect(Todo.todos[0].done).toBe(false);
-  }));
-
-  it('should remove by index', inject(function (Todo) {
-    Todo.add('A todo to remove');
-    expect(Todo.todos.length).toBe(1);
-  }));
-});
-
 describe('Tabs', function () {
 
   var elm, scope;
