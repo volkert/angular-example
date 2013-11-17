00c17b36065228b85d09128f931d17bfddd71d7f
Volker Tietz <volker.tietz@gmail.com>
2013-11-17 14:27:02 +0100

    removed angular.mock prefix for inject and module function calls

--- test/app_test.js
+++ test/app_test.js
@@ -2,20 +2,20 @@
 
 describe('Todo', function(){
 
-  beforeEach(angular.mock.module('MwayTodo'));
+  beforeEach(module('MwayTodo'));
 
-  it('should initialize with an empty todos array', angular.mock.inject(function(Todo) {
+  it('should initialize with an empty todos array', inject(function(Todo) {
     expect(Todo.todos.length).toBe(0);
   }));
 
-  it('should add todo', angular.mock.inject(function(Todo) {
+  it('should add todo', inject(function(Todo) {
     Todo.add('A todo');
     expect(Todo.todos.length).toBe(1);
     expect(Todo.todos[0].title).toEqual('A todo');
     expect(Todo.todos[0].done).toBe(false);
   }));
 
-  it('should remove by index', angular.mock.inject(function(Todo) {
+  it('should remove by index', inject(function(Todo) {
     Todo.add('A todo to remove');
     expect(Todo.todos.length).toBe(1);
   }));
